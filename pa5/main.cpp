b7e218f2fb501c46f1bc717a116a5bcb7dea2ec3
Vicente <vcntlee@umich.edu>
2015-11-19 19:56:30 -0500

    tester for heap.cpp

---
+++ pa5/main.cpp
@@ -0,0 +1,46 @@
+#include "Heap.h"
+#include <iostream>
+
+int main(){
+    Heap h(5, 0);
+
+    Node node1, node2, node3, node4, node5;
+    node1.dist = 500; node1.v = 1;
+    node2.dist = 300; node2.v = 2;
+    node3.dist = 700; node3.v = 3;
+    node4.dist = 280; node4.v = 4;
+    node5.dist = 400; node5.v = 5;
+
+    h.insertKey(node1);
+    h.insertKey(node2);
+    h.insertKey(node3);
+    h.insertKey(node4);
+    h.insertKey(node5);
+    h.print();
+
+    int minimum = h.extractMin();
+    cout << "minimum is: " << minimum << endl;
+    h.print();
+
+    minimum = h.extractMin();
+    cout << "minimum is: " << minimum <<endl;
+    h.print();
+
+    minimum = h.extractMin();
+    cout << "minimum is: " << minimum <<endl;
+    h.print();
+
+    minimum = h.extractMin();
+    cout << "minimum is: " << minimum <<endl;
+    h.print();
+
+    minimum = h.extractMin();
+    cout << "minimum is: " << minimum <<endl;
+    h.print();
+
+    minimum = h.extractMin();
+    cout << "minimum is: " << minimum <<endl;
+    h.print();
+
+}
+
