309396daec0f97c1f1474c2fea8ddd9de4bc7349
Vicente <vcntlee@umich.edu>
2015-11-19 19:27:48 -0500

    heap implementation done, need destructor

--- pa5/Heap.cpp
+++ pa5/Heap.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 Heap::Heap(int inSize, int inCurSize){
     size = inSize;
+    iter = size -1;
     curSize = inCurSize;
     container = new Node[size];
 
@@ -38,16 +39,20 @@ void Heap::initHeap(int inSize, int inSourceV){
         
 int Heap::extractMin(){
     Node minimum = container[0];
-    swap(container[0], container[curSize]);
-    container[curSize].dist = numeric_limits<int>::max(); 
+    cout << "iter " << iter <<endl;
+    swap(container[0], container[iter]);
+    container[iter].dist = numeric_limits<int>::max(); 
+    cout << "c[iter] " << container[iter].dist << endl;
 
     extractMinHelper(0);
+
+    iter--;
 
     return minimum.dist;
 }
 
 void Heap::extractMinHelper(int i){
-    if (i == curSize) return;
+    if (i == curSize-1) return;
     
     else if (container[i] > container[2*i+1] || container[i] > container[2*i+2]){
         if (container[2*i+1] < container[2*i+2]){
@@ -79,14 +84,15 @@ void Heap::insertKey(Node node){
         container[curSize] = node;
         insertKeyHelper(curSize);
     }
+
     curSize++;
+    iter = curSize-1;
 
 }        
 
 void Heap::insertKeyHelper(int i){
 
     if (i==0){
-        curSize--;
         return;
     }
 
@@ -106,5 +112,6 @@ void Heap::insertKeyHelper(int i){
 
 void Heap::print(){
     for (int i = 0; i < size; i++)
-        cout << (container[i].dist) << endl;        
+        cout << (container[i].dist) << " ";        
+    cout <<endl;
 }
