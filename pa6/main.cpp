38267d8e6ee77ace489d7314d669719a3de998f4
Vicente <vcntlee@umich.edu>
2015-11-28 15:35:59 -0500

    tester for both min-max heaps

---
+++ pa6/main.cpp
@@ -0,0 +1,29 @@
+#include "maxheap.h"
+#include "MaxHeap.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    MaxHeap maxheap(10);
+    maxheap.insertNode(8);
+    maxheap.insertNode(3);
+    maxheap.insertNode(10);
+    maxheap.insertNode(4);
+    maxheap.insertNode(20);
+    maxheap.insertNode(7);
+    maxheap.insertNode(15);
+    maxheap.insertNode(9);
+    maxheap.insertNode(19);
+    maxheap.insertNode(1);
+
+    maxheap.printMaxHeap();
+
+    for (int i = 0; i < 10; i++){
+        int maximum = maxheap.extractMax();
+        cout << "max " << maximum << endl;
+        maxheap.printMaxHeap();
+    }
+
+
+}
