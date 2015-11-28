2d69402137010dc6124b69a260a28319d8292aa5
Vicente <vcntlee@umich.edu>
2015-11-28 15:35:05 -0500

    maxheap header file

---
+++ pa6/MaxHeap.h
@@ -0,0 +1,23 @@
+#ifndef _MAXHEAP_H_
+#define _MAXHEAP_H_
+
+class MaxHeap{
+    private:
+        int *maxContainer;
+        int maxSize;
+        int tracker;
+        int curSize;
+
+    public:
+        MaxHeap(int inMaxSize);
+        void insertNode(int node);
+        void insertNodeHelper(int pos);
+        int extractMax();
+        void extractMaxHelper(int pos);
+        void swapNode(int a, int b);
+        void printMaxHeap();
+
+};
+
+#endif
+
